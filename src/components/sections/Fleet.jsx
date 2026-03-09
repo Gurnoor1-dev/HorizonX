b2cc7c1a6ce35bdddc86c02a9dee4f07bf24074b
Gurnoor1-dev <gurnoor123654@gmail.com>
2026-03-09 05:33:59 +0100

    Create Fleet.jsx

---
+++ src/components/sections/Fleet.jsx
@@ -0,0 +1,10 @@
+<GlassCard>
+
+<img src={aircraft.image} className="rounded-xl"/>
+
+<h3 className="text-xl mt-3">{aircraft.name}</h3>
+
+<p>Range: {aircraft.range}</p>
+<p>Capacity: {aircraft.capacity}</p>
+
+</GlassCard>
